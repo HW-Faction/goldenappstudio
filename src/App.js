370aa63074c71896445660eef390949e3db691b7
HW-Faction <hankwilliamson49@gmail.com>
2019-07-22 15:41:16 +0530

    navbar has been added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import PrimaryMain from "./components/MainContent/PrimaryMain";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <PrimaryMain />
       </div>
     );
   }
