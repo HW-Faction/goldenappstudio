bc5bda8bf55e81dae2c0ba26e0ecc88b155c209b
HW-Faction <hankwilliamson49@gmail.com>
2019-07-23 10:30:51 +0530

    Updates

---
+++ precache-manifest.994a84acb9a69c51f47bb75572eadcc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120c6177761395ffb9e51338bf03d136",
+    "url": "/goldenappstudio/index.html"
+  },
+  {
+    "revision": "79dd389517a7b9e63974",
+    "url": "/goldenappstudio/static/css/main.ef970cb9.chunk.css"
+  },
+  {
+    "revision": "2ce536541b626a4675ac",
+    "url": "/goldenappstudio/static/js/2.105a6ef0.chunk.js"
+  },
+  {
+    "revision": "79dd389517a7b9e63974",
+    "url": "/goldenappstudio/static/js/main.dc5474fb.chunk.js"
+  },
+  {
+    "revision": "fb9ff9b08a8f1b9be8a9",
+    "url": "/goldenappstudio/static/js/runtime~main.370585f5.js"
+  }
+]);
