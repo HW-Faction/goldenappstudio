d550707b251b3eb2027282b7888390d0910165eb
HW-Faction <hankwilliamson49@gmail.com>
2019-07-25 22:41:22 +0530

    v6.6.6

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import MainApp from "./components/MainApp/MainApp";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Contact from "./components/MainApp/Contact";
-import HEadFirst from "./components/Header/HEadFirst";
+import Contact from "./components/MainApp/Contact/Contact";
+import About from "./components/MainApp/About/About";
 
 class App extends React.Component {
   render() {
@@ -17,9 +17,10 @@ class App extends React.Component {
 
           <Link to="/contact" />
 
-          <Link to="/topics" />
+          <Link to="/about" />
           <Route exact path="/" component={MainApp} />
           <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
         </div>
         <Footer />
       </Router>
---
+++ src/components/MainApp/About/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div id="cover-spin" />
+      <section class="company-page-banner">
+        <div class="container">
+          <div class="row justify-content-center align-items-center">
+            <div class="col-12 py-3">
+              <h1 class="text-left text-white heading-banner">About Us</h1>
+              <p class="latoRegular text-left text-white description-banner">
+                Since 2008, we’ve been committed to providing dedicated remote
+                teams to engineer finest digital experiences
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
