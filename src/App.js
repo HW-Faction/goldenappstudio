33242365008faf8d2623174e5bd8c0c066346511
HW-Faction <hankwilliamson49@gmail.com>
2019-07-26 08:12:24 +0530

    v7.1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import MainApp from "./components/MainApp/MainApp";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -10,20 +10,20 @@ import About from "./components/MainApp/About/About";
 class App extends React.Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <HashRouter basename="/">
         <div>
           <Header />
           <Link to="/" />
 
-          <Link to="/goldenappstudio/contact" />
+          <Link to="/contact" />
 
-          <Link to="/goldenappstudio/about" />
+          <Link to="/about" />
           <Route exact path="/" component={MainApp} />
-          <Route path="/goldenappstudio/contact" component={Contact} />
-          <Route path="/goldenappstudio/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
         </div>
         <Footer />
-      </Router>
+      </HashRouter>
     );
   }
 }
