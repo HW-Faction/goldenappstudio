bdd7721a22ab3e01e29b472cc2dac9dc938ef049
HW-Faction <hankwilliamson49@gmail.com>
2019-07-26 10:23:38 +0530

    LEADERS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Link } from "react-router-dom";
 import MainApp from "./components/MainApp/MainApp";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Contact from "./components/MainApp/Contact/Contact";
-import About from "./components/MainApp/About/About";
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Link to="/" />
-
-          <Link to="/contact" />
-
-          <Link to="/about" />
-          <Route exact path="/" component={MainApp} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
-        </div>
+      <div>
+        <Header />
+        <MainApp />
         <Footer />
-      </BrowserRouter>
+      </div>
     );
   }
 }
